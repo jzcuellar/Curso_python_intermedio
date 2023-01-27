13eb8f4e031b8e55b7f0ef9d31d04cd9f6cc6abc
Jefferzon <jefferzon.cuellar@gmail.com>
2023-01-27 13:36:56 -0500

    Proyecto Juego del ahorcado

--- proyecto_01_juego_ahorcado/main_juego_ahorcado.py
+++ proyecto_01_juego_ahorcado/main_juego_ahorcado.py
@@ -1,13 +1,31 @@
+import sys
 import os
 import random
 
+
+def open_file_palabras():
+
+    try:
+        with open("./proyecto_01_juego_ahorcado/palabras.txt", "r", encoding="utf-8") as f:
+            lst_palabras = []
+            for line in f: 
+                lst_palabras.append(line.strip())
+            return lst_palabras
+    
+    except FileNotFoundError:
+        print("No se ha encontrado el archivo de palabras, verifica la ruta")
+        sys.exit()
+    
+    except:
+        print("Ha ocurrido un error al abrir el archivo, verifica que el archivo esta en formato utf-8 ")
+        sys.exit()
+
+
 def palabra_random():
-    lst_palabras = []
-    with open("./proyecto_01_juego_ahorcado/palabras.txt", "r", encoding="utf-8") as f:
-        for line in f: 
-            lst_palabras.append(line)
+    lst_palabras = open_file_palabras()
     palabra = random.choice(lst_palabras)
     return replace_tilde(palabra)
+
 
 def replace_tilde(string):
     vowels = {"á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u"}
@@ -29,13 +47,13 @@ def print_ongoing_game(lst_palabra,letters_user):
 
 
 def run():
+
     palabra = palabra_random()
     lst_palabra = [x for x in palabra]
     letters_user = []
-    
+
     while set(lst_palabra) != set(letters_user):
         print_ongoing_game(lst_palabra,letters_user)
-    
     clean_screen()
     print(f'Ganaste la palabra era {palabra}')
 
